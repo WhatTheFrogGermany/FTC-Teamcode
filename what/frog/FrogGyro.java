4d5622699a7c907ab20ecf915d539b562220feb7
Quewak <what.thefrog@web.de>
2017-01-24 09:01:34 +0100

    FrogGyro
    Added custom gyro class to use. This way we should be able to read several gyros at once. For now I have kind of copy pasted the code for the range sensors and then changed the Registers to read. I will try it out later and then see if it works. Otherwise I'll need to follow this tutorial.
    http://olliesworkshops.blogspot.de/2015/10/ftc-i2c-sensor-driver-architecture.html
    
    I still need to write the functions to read.
    Later I will also try out the commands to set the z_axis to zero.

---
+++ what/frog/FrogGyro.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.what.frog;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+
+/**
+ * Created by FTC2 on 24.01.2017.
+ */
+
+public class FrogGyro {
+    final static int HEADING_REGISTER = 0x04;
+    final static int INTEGRATED_Z_REGISTER = 0x06;
+    final static int RAW_X_REGISTER = 0x08;
+    final static int RAW_Y_REGISTER = 0x0a;
+    final static int RAW_Z_REGISTER = 0x0c;
+    final static int Z_AXIS_OFFSET_REGISTER = 0x0e;
+    final static int Z_AXIS_SCALING_REGISTER = 0x10;
+
+    HardwareMap hardwareMap;
+    I2cDevice i2cDevice;
+    byte[] readCache;
+    I2cDeviceSynch deviceRead;
+    int address;
+    String name;
+
+    public FrogGyro(HardwareMap hardwareMap, String name, int address){
+        this.name = name;
+        this.address = address;
+        this.hardwareMap = hardwareMap;
+    }
+    public void init(){
+        i2cDevice = hardwareMap.i2cDevice.get(name);
+        deviceRead = new I2cDeviceSynchImpl(i2cDevice, I2cAddr.create8bit(address), false);
+        deviceRead.engage();
+    }
+
+
+
+
+}
