01ea19d2f5d75e5c8f58dc7893fbc903ff9f3a11
Quewak <what.thefrog@web.de>
2017-01-26 12:39:17 +0100

    WriteFileTest output directory
    wanted to now where the files where going. It worked. Created a file. Next thing to do will beto write out arrays in csv format or something

--- WriteFileTest.java
+++ WriteFileTest.java
@@ -17,8 +17,21 @@ import java.io.IOException;
 
 @TeleOp(name="Test: writeFile", group="Tests")
 public class WriteFileTest extends OpMode {
+    File directory;
+    String fileName = "blubb.txt";
+
     @Override
     public void init() {
+        if(isExternalStorageWritable()){
+            directory = getFilesStorageDir("tests");
+            int[] array = {0};
+            telemetry.addData("directory", directory.getAbsolutePath());
+            try {
+                write(fileName, directory, array);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
@@ -49,6 +62,10 @@ public class WriteFileTest extends OpMode {
         File writeFile = new File(directory.getPath(), fileName);
         writeFile.createNewFile();
         BufferedWriter outputWriter = new BufferedWriter(new FileWriter(writeFile));
+        outputWriter.write("Blubb");
+        outputWriter.flush();
+        outputWriter.close();
 
     }
 }
+//hallo mein name ist Janine ich rieche nach head and shoulders
