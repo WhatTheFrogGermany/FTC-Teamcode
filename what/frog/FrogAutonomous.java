d3db5bea81d8a644d187efd558c9351839f36cb1
Quewak <what.thefrog@web.de>
2017-02-01 20:37:35 +0100

    FrogAutonomous changeDirection
    also enabled this to changeDirections. Again put in frontLeftDrive, rightDrive etc. as with the TeleOpOmni4.
    Will next run tests, where the motors are and then map them.
    
    NOTE TO SELF
    there are still some classes there that are derived from TeleOpOmni3. I should probably check these.

--- what/frog/FrogAutonomous.java
+++ what/frog/FrogAutonomous.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.what.frog;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by FTC2 on 01.02.2017.
  */
@@ -7,12 +9,18 @@ public class FrogAutonomous extends FrogOpMode {
     public final static short HILDE_FRONT = 0;
     public final static short FRANZ_FRONT = 1;
     public final static short BEACON_FRONT = 2;
-    public final static short LIFT_FRONT = 3;
+    public final static short GABI_FRONT = 3;
+
+    short front = GABI_FRONT;
 
     int robotX;
     int robotY;
 
-    short front;
+    FrogMotor frontRightDrive;
+    FrogMotor frontLeftDrive;
+    FrogMotor backLeftDrive;
+    FrogMotor backRightDrive;
+
     public void driveToPosition(int x, int y){
 
     }
@@ -30,6 +38,13 @@ public class FrogAutonomous extends FrogOpMode {
         return result;
     }
 
+    public void resetHeading(){
+        bottomGyro.reset();
+        topGyro.reset();
+    }
+    public int getFront(){
+        return front;
+    }
     public void pushBlueBeacon(){
 
     }
@@ -40,5 +55,11 @@ public class FrogAutonomous extends FrogOpMode {
 
     public void shootBall(){
 
+    }
+
+    public void changeDirection(int direction){
+        if(direction == GABI_FRONT){
+
+        }
     }
 }
