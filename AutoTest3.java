43119aa1470f4f2a217d455b530422969e508876
Quewak <what.thefrog@web.de>
2017-02-13 17:36:33 +0100

    test for DriveToPosition
    called it AutoTest3

--- AutoTest3.java
+++ AutoTest3.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 import org.firstinspires.ftc.teamcode.what.frog.FrogAutonomousSimpler;
 
 /**
  * Created by FTC2 on 13.02.2017.
  */
+@Autonomous(name="Test: 3", group="Tests")
 public class AutoTest3 extends FrogAutonomousSimpler {
     @Override
     public void init() {
