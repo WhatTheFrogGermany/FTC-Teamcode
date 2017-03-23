895f5dfa73b8025d1c8b2fcb08155cadb625e06a
Quewak Quak <what.thefrog@web.de>
2017-03-23 19:40:08 +0100

    started work on MeasureOmniDrive
    the file will be called "omni_drive.csv"

--- measurements/MeasureOmniDrive.java
+++ measurements/MeasureOmniDrive.java
@@ -1,7 +1,25 @@
 package org.firstinspires.ftc.teamcode.measurements;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.what.frog.FrogOpMode;
+import org.firstinspires.ftc.teamcode.what.frog.FrogTableTelemetry;
+
 /**
  * Created by FTC2 on 23.03.2017.
  */
-public class MeasureOmniDrive {
+@TeleOp(name="Measure: OmniDrive", group="Measure")
+public class MeasureOmniDrive extends FrogOpMode {
+    FrogTableTelemetry tableWriter;
+    ElapsedTime elapsedTime;
+    double ratio = 1;
+    double ratioSteps;
+
+    @Override
+    public void init() {
+        super.init();
+        elapsedTime = new ElapsedTime();
+        tableWriter = new FrogTableTelemetry("omni_drive.csv", 9, "omni",telemetry);
+    }
 }
