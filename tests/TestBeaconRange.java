d2b40e0c33e805a18a57fd959ba5dff6b5eada66
Quewak Quak <what.thefrog@web.de>
2017-04-03 10:12:22 +0200

    Test Beacon Range
    just returns the values for the ultrasonic reading. Will now try it with different parameters and write down when something does not work.

---
+++ tests/TestBeaconRange.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.what.frog.FrogOpMode;
+
+/**
+ * Created by FTC2 on 03.04.2017.
+ */
+@TeleOp(name="Test: Beacon Range", group="Tests")
+public class TestBeaconRange extends FrogOpMode {
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+        telemetry.addData("leftBeacon", leftBeaconRange.getUltrasonic());
+        telemetry.addData("rightBeacon", rightBeaconRange.getUltrasonic());
+    }
+}
